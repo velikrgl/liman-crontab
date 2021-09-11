455d5f5ea1593b08037e5093d1768dd7740012dd
velikrgl <velikrgl@yahoo.com>
2021-09-11 12:12:06 +0300

    dropmenu for common usage

--- views/crontab/main.blade.php
+++ views/crontab/main.blade.php
@@ -1,26 +1,28 @@
-<div class="row">
-    <div class="col-12 mb-2">
-        <b>{{ __('Hostname') }}</b>: <span class="hostname"></span>
-    </div>
-    <div class="col-12">
-        @include('modal-button',[
-            "class"     => "btn btn-primary mb-2",
-            "target_id" => "setHostnameModal",
-            "text"      => "Hostname Değiştir",
-            "icon"      => "fas fa-plus"
-        ])
 
-        @include('modal',[
-            "id" => "setHostnameModal",
-            "title" => "Hostname Değiştir",
-            "url" => API('set_hostname'),
-            "next" => "getHostname",
-            "inputs" => [
-                "Hostname" => "hostname:text"
-            ],
-            "submit_text" => "Hostname Değiştir"
-        ])
-    </div>
+<div class="dropdown">
+  <h5>Ortak Ayarlarlardan Ekle  </h5>
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+  style="background-color:white; color:black; "
+  >  
+  {{ __("Ortak Ayarlar") }}
+  </button>
+ 
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Once Per Minute (* * * * *)</a>
+    <a class="dropdown-item" href="#">Once Per Five Minute (*/5 * * * *)</a>
+    <a class="dropdown-item" href="#">Twice per Hour (0,30 * * * *)</a>
+    <a class="dropdown-item" href="#">One per Hour (0 * * * *)</a>
+    <a class="dropdown-item" href="#">Twice per Day (0 0,12 * * *)</a>
+    <a class="dropdown-item" href="#">Once per Day (0 0 * * *)</a>
+    <a class="dropdown-item" href="#">Once per Week (0 0 * * 0)</a>
+    <a class="dropdown-item" href="#">Once per Month (0 0 1 * *)</a>
+    <a class="dropdown-item" href="#">Once per Year (0 0 1 1 *)</a>
+    
+  </div>
 </div>
 
-@include("hostname.scripts")
+
+
+@include("crontab.scripts")
+
