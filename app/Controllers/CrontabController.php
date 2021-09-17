1ec99bff9062d9de92c8ae8680039e3d1a0c3a72
velikrgl <43519511+velikrgl@users.noreply.github.com>
2021-09-17 18:18:40 +0300

    catch the requests from inputs

--- app/Controllers/CrontabController.php
+++ app/Controllers/CrontabController.php
@@ -5,11 +5,24 @@ use Liman\Toolkit\Shell\Command;
 
 class CrontabController
 {
-//echo "* * * * * echo "veli" >/dev/pts/0" >> /var/spool/cron/crontabs/$USER
-	public function CalCommand($min,$hour,$dayOfMonth,$month,$dayOfWeek,$userCommand)
+// echo "* * * * * echo "veli" >/dev/pts/0" >> /var/spool/cron/crontabs/$USER
+// echo "@{:newMinute} @{:newHour} @{:newMday} @{:newmonth} @{:newWday} @{:newcommand}" >> /var/spool/cron/crontabs/$USER  
+	public function CalCommand()
 	{
-		
-		$result = Command::runSudo('');
 
+		//$result = ;
+		$output = Command::runSudo('echo "@{:newMinute} @{:newHour} @{:newMday} @{:newmonth} @{:newWday} @{:newcommand}" >> /var/spool/cron/crontabs/$USER',
+        [
+			
+            "newMinute" => request("newMinute"),
+			"newHour" => request("newHour"),
+			"newMday" => request("newMday"),
+			"newmonth" => request("newmonth"),
+			"newWday" => request("newWday"),
+			"newcommand" => request("newcommand"),
+
+        ]);
+		return respond($output,200);
 	}
+	
 }
